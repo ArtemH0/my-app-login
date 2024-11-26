667d35146cfa133827fbf26dee71321c7c8d5467
ArtemH0 <haustovich.artem@gmail.com>
2024-11-26 21:09:09 +0200

    login change

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,47 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import TodoList from "../../components/Todo/TodoList";
 import TodoForm from "../../components/Todo/TodoForm";
-import "./HomePage.css"
+import "./HomePage.css";
 
 const HomePage = () => {
   const [todos, setTodos] = useState([]);
-  const userId = localStorage.getItem("currentUserId")
-  
-  useEffect(() => {
-    const userTodos = retrieveUserTodos()
-    console.log('userTodos: ', userTodos);
+  const userId = localStorage.getItem("currentUserId");
 
+  // Отримати всі збережені todos
+  const retrieveTodos = useCallback(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    return savedTodos;
+  }, []);
+
+  // Отримати todos для поточного користувача
+  const retrieveUserTodos = useCallback(() => {
+    const savedTodos = retrieveTodos();
+    const currentUserTodos = savedTodos.find(
+      (userTodo) => userTodo.id === userId
+    );
+    return currentUserTodos?.todos || [];
+  }, [retrieveTodos, userId]);
+
+  useEffect(() => {
+    const userTodos = retrieveUserTodos();
     if (userTodos) {
-      setTodos(userTodos)
+      setTodos(userTodos);
     }
-  }, [])
+  }, [retrieveUserTodos]);
 
   const addTodo = (text) => {
     const newTodo = { text, id: Date.now(), completed: false };
     const newTodos = [...todos, newTodo];
-    console.log('newTodos: ', newTodos);
     saveTodos(newTodos);
   };
-  
+
   const saveTodos = (todos) => {
     setTodos(todos);
 
-    const savedTodos = retrieveTodos()
-
+    const savedTodos = retrieveTodos();
     const updatedTodos = savedTodos.map((userTodo) => {
       if (userTodo.id === userId) {
-        return { id: userId, todos: todos }
+        return { id: userId, todos: todos };
       } else {
-        return userTodo
+        return userTodo;
       }
-    })
-    
+    });
+
     if (!updatedTodos.find((userTodo) => userTodo.id === userId)) {
-      updatedTodos.push({ id: userId, todos: todos })
+      updatedTodos.push({ id: userId, todos: todos });
     }
 
-    localStorage.setItem("todos", JSON.stringify(updatedTodos))
-  }
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
 
   const toggleTodo = (id) => {
     const updatedTodos = todos.map((todo) => {
@@ -57,22 +68,6 @@ const HomePage = () => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     saveTodos(updatedTodos);
   };
-
-  const retrieveTodos = () => {
-    const savedTodos = JSON.parse(localStorage.getItem("todos")) || []
-    
-    return savedTodos
-  }
-
-  const retrieveUserTodos = () => {
-    const savedTodos = retrieveTodos()
-
-    const currentUserTodos = savedTodos.find((userTodo) => {
-      return userTodo.id === userId
-    })
-
-    return currentUserTodos?.todos
-  } 
 
   return (
     <div className="home-page">
